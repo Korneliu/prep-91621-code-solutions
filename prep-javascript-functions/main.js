d3e156654cb6686359c3e9b3a951af7e270cc5b7
Korneliu <olbiton@gmail.com>
2021-09-29 14:27:55 -0700

    completed prep-javascript-functions

--- prep-javascript-functions/main.js
+++ prep-javascript-functions/main.js
@@ -31,6 +31,36 @@ function multiplyAndDivide(num1, num2) {
   return (num1 * num2) / 5;
 }
 
-var multiplyDivide = multiplyAndDivide(2, 2);
+var multiplyDivide = multiplyAndDivide(5, 5);
 console.log('multiply and divide by 5 :', multiplyDivide)
 ;
+
+function substractTwoNumbers(num1, num2) {
+  return num1 - num2;
+}
+
+var substract = substractTwoNumbers(9, 6);
+console.log('substract two numbers: ', substract);
+
+function getCircleCircumference(number) {
+  return 2 * 3.1415926535 * number;
+}
+
+var circumreference = getCircleCircumference(5);
+console.log('circumreference: ', circumreference)
+;
+
+function getFullname(firstName, lastName) {
+  return firstName + ' ' + lastName;
+}
+
+var fullName = getFullname('Max', 'Cendalski');
+console.log('full name: ', fullName)
+;
+
+function cube(number) {
+  return number * number * number;
+}
+
+var getCube = cube(5);
+console.log('cube: ', getCube);
