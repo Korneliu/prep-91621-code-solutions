009d6ced20f51fec52cc60ed792c8bcb387e6215
Korneliu <olbiton@gmail.com>
2021-09-30 17:24:56 -0700

    completed prep-javascript-for-loops

---
+++ prep-javascript-for-loops/main.js
@@ -0,0 +1,15 @@
+// create your loops here.
+console.log('Loop 1');
+for (var i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+console.log('Loop 2');
+for (var i = 0; i < 20; i = i + 2) {
+  console.log(i);
+}
+
+console.log('Loop 3');
+for (var i = 100; i >= 0; i = i - 1) {
+  console.log('Time till explosion', i, '!');
+}
